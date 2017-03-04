45b1bc18ca224200bb80c6501401d6d47c1d0fc7
Matthew D. Steele <mdsteele@alum.mit.edu>
2017-03-03 21:46:22 -0500

    Add more crate-level documentation and example code

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,59 @@
 //!
 //! The API of this crate is meant to be similar to that of the
 //! [`tar`](https://crates.io/crates/tar) crate.
+//!
+//! # Format variants
+//!
+//! Unix archive files come in several variants, of which three are the most
+//! common:
+//!
+//! * The *common variant*, used for Debian package (`.deb`) files among other
+//!   things, which only supports filenames up to 16 characters.
+//! * The *BSD variant*, used by the `ar` utility on BSD systems (including Mac
+//!   OS X), which is backwards-compatible with the common variant, but extends
+//!   it to support longer filenames and filenames containing spaces.
+//! * The *GNU variant*, used by the `ar` utility on GNU and many other systems
+//!   (including Windows), which is similar to the common format, but which
+//!   stores filenames in a slightly different, incompatible way, and has its
+//!   own strategy for supporting long filenames.
+//!
+//! Currently, this crate supports reading and writing the BSD/common variant,
+//! but does not support the GNU variant at all.
+//!
+//! # Example usage
+//!
+//! Writing an archive:
+//!
+//! ```no_run
+//! use ar::Builder;
+//! use std::fs::File;
+//! // Create a new archive that will be written to foo.a:
+//! let mut builder = Builder::new(File::create("foo.a").unwrap());
+//! // Add foo/bar.txt to the archive, under the name "bar.txt":
+//! builder.append_path("foo/bar.txt").unwrap();
+//! // Add foo/baz.txt to the archive, under the name "hello.txt":
+//! let mut file = File::open("foo/baz.txt").unwrap();
+//! builder.append_file("hello.txt", &mut file).unwrap();
+//! ```
+//!
+//! Reading an archive:
+//!
+//! ```no_run
+//! use ar::Archive;
+//! use std::fs::File;
+//! use std::io;
+//! // Read an archive from the file foo.a:
+//! let mut archive = Archive::new(File::open("foo.a").unwrap());
+//! // Iterate over all entries in the archive:
+//! while let Some(entry_result) = archive.next_entry() {
+//!     let mut entry = entry_result.unwrap();
+//!     // Create a new file with the same name as the archive entry:
+//!     let mut file = File::create(entry.header().identifier()).unwrap();
+//!     // The Entry object also acts as an io::Read, so we can easily copy the
+//!     // contents of the archive entry into the file:
+//!     io::copy(&mut entry, &mut file).unwrap();
+//! }
+//! ```
 
 #![warn(missing_docs)]
 
