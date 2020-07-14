accd314ea6e82b6dd9a5a477a75421d215f8a6c6
Greg Bowyer <greg.bowyer@ticketmaster.com>
2020-07-14 08:29:28 -0700

    WIP: Update examples/symbols.rs
    Co-authored-by: bjorn3 <bjorn3@users.noreply.github.com>

--- examples/symbols.rs
+++ examples/symbols.rs
@@ -26,6 +26,6 @@ fn main() {
     let mut archive = ar::Archive::new(input_file);
 
     for symbol in archive.symbols().expect("failed to parse symbols") {
-        println!("{}", symbol.name().expect("symbol name is not UTF-8"));
+        println!("{}", String::from_utf8_lossy(symbol.raw_name());
     }
 }
